8109a20a2a64e48f6a1fea09c6665b2b3dee17f8
changnam <changnam.go@gmail.com>
2022-09-08 22:17:01 +0900

    plugin update for displaying java system properties

--- src/main/java/com/honsoft/GreetingMojo.java
+++ src/main/java/com/honsoft/GreetingMojo.java
@@ -1,5 +1,8 @@
 package com.honsoft;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -28,8 +31,31 @@ public class GreetingMojo extends AbstractMojo
     public void execute() throws MojoExecutionException
     {
         getLog().info(greeting );
+        
+        Map<String,Object> envs = new HashMap<>();
+		envs.put("file.separator", System.getProperty("file.separator"));
+		envs.put("java.class.path", System.getProperty("java.class.path"));
+		envs.put("java.home", System.getProperty("java.home"));
+		envs.put("java.vendor", System.getProperty("java.vendor"));
+		envs.put("java.vendor.url", System.getProperty("java.vendor.url"));
+		envs.put("java.version", System.getProperty("java.version"));
+		envs.put("line.separator", System.getProperty("line.separator"));
+		envs.put("os.arch", System.getProperty("os.arch"));
+		envs.put("os.name", System.getProperty("os.name"));
+		envs.put("os.version", System.getProperty("os.version"));
+		envs.put("path.separator", System.getProperty("path.separator"));
+		envs.put("user.dir", System.getProperty("user.dir"));
+		envs.put("user.home", System.getProperty("user.home"));
+		envs.put("user.name", System.getProperty("user.name"));
+		
+		for (String key : envs.keySet()){
+			getLog().info(key+" : "+envs.get(key));
+		}
+		
         for(String array : myArray) {
         	getLog().info(array);
         }
+        
+        
     }
 }
